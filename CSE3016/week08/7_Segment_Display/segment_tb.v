e1d2be926697251f122a5579de22e9df3a5ae398
hyerin <mye785c83@gmail.com>
2021-01-23 14:23:20 +0900

    complete week8

--- CSE3016/week08/7_Segment_Display/segment_tb.v
+++ CSE3016/week08/7_Segment_Display/segment_tb.v
@@ -1,24 +1,28 @@
 `timescale 1ns / 1ps
 
-module segment_sim();
-reg clk,a,b,c,d;
-wire A,B,C,D,E,F,G;
+module segment_tb();
+    reg clk,a,b,c,d;
+    wire A,B,C,D,E,F,G;
 
-segment connect(
-    .a(a), .b(b), .c(c), .d(d),
-    .A(A), .B(B), .C(C), .D(D), .E(E), .F(F), .G(G)
-);
+    segment connect(
+        .a(a), .b(b), .c(c), .d(d),
+        .A(A), .B(B), .C(C), .D(D), .E(E), .F(F), .G(G)
+    );
 
-initial begin
-    clk = 0 ; a = 0 ; b = 0 ; c = 0; d = 0;
-end
+    initial begin
+        clk = 1; 
+        a = 0; 
+        b = 0; 
+        c = 0; 
+        d = 0;
+    end
 
-always clk = #10 ~clk;
-always @(posedge clk)begin
- d<= ~d;
- c <= #40 ~c;
- b <= #80 ~b;
- a <= #160 ~a;
- end
-
+    always clk = #10 ~clk;
+    
+    always @(posedge clk)begin
+        d<= ~d;
+        c <= #20 ~c;
+        b <= #60 ~b;
+        a <= #140 ~a;
+    end
 endmodule
